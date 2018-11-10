78f0ac429a8926bc990e9eb7b6dc6c6e49545263
LIKK <admin@office.lehniner-institut-fuer-kunst.de>
2018-11-10 20:59:39 +0100

    Wagon Creation

--- app_root.rb
+++ app_root.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 
 # define the root directory of the main application
-ENV['APP_ROOT'] ||= File.expand_path('../../hitobito', __FILE__)
+ENV['APP_ROOT'] ||= File.expand_path('../hitobito', __FILE__)
